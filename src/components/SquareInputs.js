c40a846dabda7301da6545dc2d589622d3a5e11e
Cristian <cris.munoz0509@gmail.com>
2021-10-15 23:52:47 -0500

    update

--- src/components/SquareInputs.js
+++ src/components/SquareInputs.js
@@ -19,13 +19,13 @@ const SquareInputs = ({onCalc}) => {
                     <input type="text" value={length} onChange={(e) => setLength(e.target.value)} ></input>
                 </div>
 
-                <svg id="shape-display" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-                    <text x="22.5" y="2.5" style={{fontSize: "3px", textAnchor: "middle"}}>{length}</text>
-                    <text x="0" y="20" style={{fontSize: "3px", textAnchor: "end"}}>{length}</text>
-                    <line x1="5" y1="4" x2="40" y2="4" stroke="black" />
-                    <line x1="5" y1="35" x2="40" y2="35" stroke="black" />
-                    <line x1="5.5" y1="4" x2="5.5" y2="35" stroke="black" />
-                    <line x1="39.5" y1="4" x2="39.5" y2="35" stroke="black" />
+                <svg id="shape-display" viewBox="6 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <text x="13.5" y="2.5" style={{fontSize: "3px", textAnchor: "middle"}}>{length}</text>
+                    <text x="-6.5" y="20" style={{fontSize: "3px", textAnchor: "end"}}>{length}</text>
+                    <line x1="-3.5" y1="4" x2="31.5" y2="4" stroke="black" />
+                    <line x1="-3.5" y1="35" x2="31.5" y2="35" stroke="black" />
+                    <line x1="-3" y1="4" x2="-3" y2="35" stroke="black" />
+                    <line x1="31" y1="4" x2="31" y2="35" stroke="black" />
                 </svg>
 
                 <div>
