ff79a0dde90cdf95ee81e3be28c321d3b6680444
Cristian <cris.munoz0509@gmail.com>
2021-10-15 20:04:41 -0500

    updateeet ete t

--- src/components/SquareInputs.js
+++ src/components/SquareInputs.js
@@ -18,6 +18,16 @@ const SquareInputs = ({onCalc}) => {
                     <label>Length/Width: </label>
                     <input type="text" value={length} onChange={(e) => setLength(e.target.value)} ></input>
                 </div>
+
+                <svg id="shape-display" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                    <text x="22.5" y="2.5" style={{fontSize: "3px", textAnchor: "middle"}}>{length}</text>
+                    <text x="0" y="20" style={{fontSize: "3px", textAnchor: "end"}}>{length}</text>
+                    <line x1="5" y1="4" x2="40" y2="4" stroke="black" />
+                    <line x1="5" y1="35" x2="40" y2="35" stroke="black" />
+                    <line x1="5.5" y1="4" x2="5.5" y2="35" stroke="black" />
+                    <line x1="39.5" y1="4" x2="39.5" y2="35" stroke="black" />
+                </svg>
+
                 <div>
                     <input type="submit" value="Calculate"></input>
                 </div>
